2fc5f7d9cca93ac088af56098b7af5eae02608f2
DESKTOP-Q6I58F0\Charles Paradzayi <cparadzayi@gmail.com>
2017-06-14 22:39:35 +0200

    implement session logic

--- config/app.js
+++ config/app.js
@@ -2,9 +2,14 @@
 module.exports.mount = app => {
     const bodyParser = require('body-parser')
     const logger = require('morgan')
-    
+    const cookieParser = require('cookie-parser')
+    const expressSession = require('express-session')
+
     // app wide middleware //
     app.use(bodyParser.urlencoded({extended: false}))
     app.use(bodyParser.json())
     app.use(logger('dev'))
+    app.use(cookieParser())
+    app.use(expressSession({ secret: process.env.EXPRESS_SESSION_SECRET}))
+    app.use(pasport.session())
 }
